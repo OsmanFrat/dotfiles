55baba82733e4620a2d451f2ea51a53a6e164c4f
OsmanFrat <osmanfrat@yahoo.com>
2025-02-23 19:06:00 +0300

    Auto commit - Sun Feb 23 07:06:00 PM +03 2025

--- config/nvim/lua/custom/mappings.lua
+++ config/nvim/lua/custom/mappings.lua
@@ -5,7 +5,7 @@ local M = {}
 M.mappings = {
   n = {
     -- <leader>ac kısayolu ile scripti çalıştır
-    ["<leader>ac"] = { ":!~/dotfiles/scripts/auto-commit.sh<CR>", "Run auto-commit script" },
+    ["<leader>ga"] = { ":!~/dotfiles/scripts/auto-commit.sh<CR>", "Run auto-commit script" },
   },
 }
 
