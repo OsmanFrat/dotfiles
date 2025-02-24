ec6aa23f7f1c8865bdbcadc7022d3343e55e47e9
OsmanFrat <osmanfrat@yahoo.com>
2025-02-24 18:08:00 +0300

    Auto commit - Mon Feb 24 06:08:00 PM +03 2025

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -3,3 +3,5 @@ c.url.searchengines = {
     'DEFAULT': 'https://www.google.com/search?q={}'
 }
 c.url.start_pages = "file:///home/ozu/GitHub/ozu-web/index.html"
+c.content.javascript.can_access_clipboard = True
+config.bind('jk', 'mode-leave', mode='insert')
