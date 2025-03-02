ed02c80bbabf2507c2b90eca22501fd16097f36a
OsmanFrat <osmanfrat@yahoo.com>
2025-03-03 01:28:01 +0300

    Auto commit - Mon Mar  3 01:28:01 AM +03 2025

--- dotfiles-install-script.sh
+++ dotfiles-install-script.sh
@@ -8,4 +8,4 @@ echo "Updating system"
 sudo pacman -Syu
 
 echo "Installing..."
-sudo pacman -S nvim vim yazi rofi cronie emacs ttf-jetbrains-mono-nerd ttf-firacode zsh starship alacritty 
+sudo pacman -S nvim vim yazi rofi cronie emacs ttf-jetbrains-mono-nerd ttf-firacode zsh starship alacritty qutebrowser firefox waybar hyprpaper less ripgrep lsd bat fzf aria2 jq fd wl-clipboard syncplay pyside6 python-adblock 
