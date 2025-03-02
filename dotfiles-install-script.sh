5415263de6bc47697a53f88908bb74d79ff922d4
OsmanFrat <osmanfrat@yahoo.com>
2025-03-03 01:40:00 +0300

    Auto commit - Mon Mar  3 01:40:00 AM +03 2025

--- dotfiles-install-script.sh
+++ dotfiles-install-script.sh
@@ -2,20 +2,30 @@
 
 #### install programs ####
 echo "Installation started..."
-cd 
 
-echo "Updating system"
-sudo pacman -Syu
+# Home dizinine gitmek
+cd ~
 
+# Sistemi güncelle
+echo "Updating system..."
+sudo pacman -Syu --noconfirm
+
+# Uygulamaları yükle
 echo "Installing apps..."
-sudo pacman -S nvim vim neofetch yazi rofi cronie emacs ttf-jetbrains-mono-nerd ttf-firacode zsh starship alacritty qutebrowser firefox waybar hyprpaper less ripgrep lsd bat fzf aria2 jq fd wl-clipboard syncplay pyside6 python-adblock mpv vlc sed curl grep yt-dlp ffmpeg patch
+sudo pacman -S --noconfirm nvim vim neofetch yazi rofi cronie emacs ttf-jetbrains-mono-nerd ttf-firacode zsh starship alacritty qutebrowser firefox waybar hyprpaper less ripgrep lsd bat fzf aria2 jq fd wl-clipboard syncplay pyside6 python-adblock mpv vlc sed curl grep yt-dlp ffmpeg patch
 
+# Yay'ı yükle
 echo "Installing yay..."
-sudo pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+sudo pacman -S --needed --noconfirm git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm
 
+# Yay ile uygulamaları yükle
 echo "Installing yay apps..."
-yay -S ani-cli hyprshot
+yay -S --noconfirm ani-cli hyprshot
 
+# Doom Emacs'i yükle
 echo "Installing doom emacs..."
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
 ~/.config/emacs/bin/doom install
+
+echo "Installation completed!"
+
