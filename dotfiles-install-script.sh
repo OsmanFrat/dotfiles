8965947803d472f347549011fff3c0e1b3316dac
OsmanFrat <osmanfrat@yahoo.com>
2025-03-03 01:44:00 +0300

    Auto commit - Mon Mar  3 01:44:00 AM +03 2025

--- dotfiles-install-script.sh
+++ dotfiles-install-script.sh
@@ -9,7 +9,7 @@ echo "Updating system..."
 sudo pacman -Syu --noconfirm
 
 echo "Installing apps..."
-sudo pacman -S --noconfirm nvim vim neofetch yazi rofi cronie emacs ttf-jetbrains-mono-nerd ttf-firacode zsh starship alacritty qutebrowser firefox waybar hyprpaper less ripgrep lsd bat fzf aria2 jq fd wl-clipboard syncplay pyside6 python-adblock mpv vlc sed curl grep yt-dlp ffmpeg patch
+sudo pacman -S --noconfirm nvim vim neofetch yazi rofi cronie emacs ttf-jetbrains-mono-nerd ttf-firacode zsh starship alacritty qutebrowser firefox waybar hyprpaper less ripgrep lsd bat fzf aria2 jq fd wl-clipboard syncplay pyside6 python-adblock mpv vlc sed curl grep yt-dlp ffmpeg patch github-cli steam
 
 echo "Installing yay..."
 sudo pacman -S --needed --noconfirm git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm
@@ -23,6 +23,12 @@ git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
 
 echo "Installing dotfiles..."
 git clone https://github.com/OsmanFrat/dotfiles
+
+mkdir -p Pictures
+stow -t ~/.config config
+stow -t $HOME shell
+stow -t ~/Pictures Pictures
+
 
 
 echo "Installation completed!"
