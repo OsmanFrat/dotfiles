c0cebf55c8e7e1a6c54900cfe90d9d6fa97939fb
OsmanFrat <osmanfrat@yahoo.com>
2025-03-03 11:00:00 +0300

    Auto commit - Mon Mar  3 11:00:00 AM +03 2025

--- dotfiles-install-script.sh
+++ dotfiles-install-script.sh
@@ -11,6 +11,9 @@ sudo pacman -Syu --noconfirm || { echo "Pacman update failed"; exit 1; }
 echo "Installing apps..."
 sudo pacman -S --noconfirm neovim vim fastfetch yazi rofi cronie npm go emacs ttf-jetbrains-mono-nerd ttf-fira-code noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra noto-fonts-emoji zsh starship alacritty qutebrowser firefox waybar hyprpaper less ripgrep lsd bat fzf aria2 jq fd wl-clipboard syncplay pyside6 python-adblock mpv vlc sed curl grep yt-dlp ffmpeg patch github-cli steam pavucontrol  || { echo "Pacman install failed"; exit 1; }
 
+echo "Changing shell to zsh..."
+chsh -s /bin/zsh
+
 echo "Installing yay..."
 sudo pacman -S --needed --noconfirm git base-devel || { echo "Pacman base-devel failed"; exit 1; }
 git clone https://aur.archlinux.org/yay.git || { echo "Yay cloning failed"; exit 1; }
