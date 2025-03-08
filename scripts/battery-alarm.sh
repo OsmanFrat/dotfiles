2c66ea7b9a56f2f48a4e4668a4b885b5ff093617
OsmanFrat <osmanfrat@yahoo.com>
2025-03-08 23:14:00 +0300

    Auto commit - Sat Mar  8 11:14:00 PM +03 2025

--- scripts/battery-alarm.sh
+++ scripts/battery-alarm.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-# Batarya yüzdesini al
 battery_percent=$(upower -i $(upower -e | grep 'BAT') | grep percentage | awk '{print $2}' | tr -d '%')
 
-# Şarj durumu al (charging/discharging)
 charging_status=$(upower -i $(upower -e | grep 'BAT') | grep state | awk '{print $2}')
 
-# Eğer şarj olmuyorsa ve batarya %20 veya daha azsa alarm çal
 if [ "$charging_status" != "charging" ] && [ "$battery_percent" -le 20 ]; then
   notify-send -u critical "Low battery! $battery_percent %"
   mpv /home/ozu/dotfiles/scripts/2.m4a
-
 fi
 
