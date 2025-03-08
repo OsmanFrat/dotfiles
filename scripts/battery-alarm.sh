3ac19a14bca2e220b45546c783d2e92f9ec747d0
OsmanFrat <osmanfrat@yahoo.com>
2025-03-08 22:16:00 +0300

    Auto commit - Sat Mar  8 10:16:00 PM +03 2025

--- scripts/battery-alarm.sh
+++ scripts/battery-alarm.sh
@@ -2,7 +2,7 @@
 
 battery_persent=$(upower -i $(upower -e | grep 'BAT') | grep percentage | awk '{print $2}' | tr -d '%')
 
-if [ "$battery_persent" -le 60 ]; then
+if [ "$battery_persent" -le 20 ]; then
   notify-send -u critical "Low battery! $battery_persent %"
   mpv ./2.m4a
 fi
