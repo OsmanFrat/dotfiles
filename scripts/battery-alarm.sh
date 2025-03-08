cc0823a7dbc3c7389c5979f814f5149ff34fb334
OsmanFrat <osmanfrat@yahoo.com>
2025-03-08 21:18:00 +0300

    Auto commit - Sat Mar  8 09:18:00 PM +03 2025

--- scripts/battery-alarm.sh
+++ scripts/battery-alarm.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 battery_persent = upower -i $(upower -e | grep 'BAT') | grep percentage | awk '{print $2}' | tr -d '%'
+
+if [ $battery_persent <= 20 ]; then
+  echo "ok"
+fi
+  echo "end"
