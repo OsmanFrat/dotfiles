bb8e73500df69628e8bc845c56225b467c441072
OsmanFrat <osmanfrat@yahoo.com>
2025-03-13 18:04:00 +0300

    Auto commit - Thu Mar 13 06:04:00 PM +03 2025

--- scripts/linkhandler.sh
+++ scripts/linkhandler.sh
@@ -17,7 +17,7 @@ fi
 case "$url" in
     *mkv*|*webm*|*mp4*|*youtube.com/watch*|*youtube.com/playlist*|*youtube.com/shorts*|*youtu.be*|*hooktube.com*|*bitchute.com*|*videos.lukesmith.xyz*|*odysee.com*)
         # Run yt-dlp in the background and suppress output
-        setsid -f yt-dlp -f 'bestvideo[height<=720]+bestaudio/best' "$url" -o - | mpv --cache=yes --force-seekable=yes --no-cache-pause  >/dev/null 2>&1 & ;;    *png*|*jpg*|*jpeg*|*gif*)
+        setsid -f yt-dlp -f 'bestvideo[height<=720]+bestaudio/best' "$url" -o - | mpv --cache=yes --force-seekable=yes --no-cache-pause - >/dev/null 2>&1 & ;;    *png*|*jpg*|*jpeg*|*gif*)
         curl -sL "$url" -o "/tmp/image" && feh "/tmp/image" >/dev/null 2>&1 & ;;
     *pdf*|*cbz*|*cbr*)
         curl -sL "$url" -o "/tmp/document" && zathura "/tmp/document" >/dev/null 2>&1 & ;;
