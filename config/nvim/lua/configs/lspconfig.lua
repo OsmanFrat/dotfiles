806dcde60d16566610c6bb9c4543c0a0143cfaa5
OsmanFrat <osmanfrat@yahoo.com>
2025-03-14 18:24:00 +0300

    Auto commit - Fri Mar 14 06:24:00 PM +03 2025

--- config/nvim/lua/configs/lspconfig.lua
+++ config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "clangd", "pyright", "gopls", "lua-language-server"}
+local servers = { "html", "cssls", "clangd", "pyright", "gopls", "lua-ls"}
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
