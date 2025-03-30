a545e6eaea779193bbdb6fdf2396c941f0addf7d
OsmanFrat <osmanfrat@yahoo.com>
2025-03-30 11:30:01 +0300

    Auto commit - Sun Mar 30 11:30:01 AM +03 2025

--- config/nvim/lua/mappings.lua
+++ config/nvim/lua/mappings.lua
@@ -7,7 +7,7 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("i", "JK", "<ESC>")
-map("n", "<D-i>", "<cmd>lua require('nvchad.term').toggle()<CR>", opts)
+map("n", "<C-D-i>", "<cmd>sp | term<CR>")
 
 -- F5 tuşu ile VimtexCompile komutunu çalıştırma
 map('n', '<F5>', ':VimtexCompile<CR>', { noremap = true, silent = true })
