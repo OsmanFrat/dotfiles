a3c532f109f44fd55843f5671f847b625030d0ea
OsmanFrat <osmanfrat@yahoo.com>
2025-04-18 13:24:00 +0300

    Auto commit - Fri Apr 18 01:24:00 PM +03 2025

--- old-configs/ozu-emacs/.emacs.d/elpa/cycbuf-20131203.2037/cycbuf-pkg.el
+++
@@ -1,8 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "cycbuf" "20131203.2037"
-  "Cycle buffers, inspired by swbuff.el, swbuff-x.el, and bs.el."
-  ()
-  :url "https://github.com/martinp26/cycbuf"
-  :commit "1079b41c3eb27d65b66d4399959bb6253f84858e"
-  :revdesc "1079b41c3eb2"
-  :keywords '("files" "convenience" "buffer switching"))
