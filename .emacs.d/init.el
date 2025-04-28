3f257d9a178479889d39b84d85fb31833f66f41c
OsmanFrat <osmanfrat@yahoo.com>
2025-04-28 16:50:01 +0300

    Auto commit - Mon Apr 28 04:50:01 PM +03 2025

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,9 +12,9 @@
   (package-install 'use-package))
 
 (setq use-package-always-ensure t)
-(require 'use-package)
+(require 'use-package);; Temel ayarlar
 
-;; Temel ayarlar
+
 (setq inhibit-startup-message t)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
@@ -37,20 +37,30 @@
           (lambda ()
             (setq mode-line-format nil)))
 
+
+
 (setq default-frame-alist '((font . "JetBrainsMono Nerd Font-15")))
+; (setq default-frame-alist '((font . "Fira code-15")))
 
 ;; Sadece programlama modlarında aç
 (add-hook 'prog-mode-hook (lambda ()
                             (display-line-numbers-mode 1)
                             (setq display-line-numbers-type 'relative)))
 
+
+
+;; Vim kısayolları
+;; (use-package evil
+;;   :init (evil-mode 1))
+
 ;; Vim emülasyonu ve katlama özellikleri
 (use-package evil
   :ensure t
   :init
-  (setq evil-want-keybinding nil)
+  (setq evil-want-keybinding nil) ; evil-collection ile uyumluluk
   :config
   (evil-mode 1))
+
 
 (evil-define-key 'normal dired-mode-map
   "h" 'dired-up-directory
@@ -62,16 +72,18 @@
   "D" 'dired-do-delete
   "n" 'dired-create-directory
   "a" 'dired-create-empty-file
-  "r" 'dired-do-rename
-  "c" 'dired-do-copy
-  "m" 'dired-mark
-  "u" 'dired-unmark
+  "r" 'dired-do-rename  ;; taşı/kes
+  "c" 'dired-do-copy    ;; kopyala
+  "m" 'dired-mark       ;; işaretle
+  "u" 'dired-unmark     ;; işareti kaldır
   "q" 'kill-buffer-and-window)
+
 
 (use-package vimish-fold
   :ensure t
   :config
-  (vimish-fold-global-mode 1)
+  (vimish-fold-global-mode 1) ; Global olarak etkinleştir
+  ;; İsteğe bağlı: Katlama görünümünü özelleştirme
   (setq vimish-fold-indication-mode 'right-fringe))
 
 (use-package evil-vimish-fold
@@ -86,23 +98,25 @@
   (key-chord-mode 1)
   (key-chord-define evil-insert-state-map "jk" 'evil-normal-state))
 
+
 ;; Tema
 (use-package doom-themes
   :config (load-theme 'doom-monokai-pro t))
 
 (use-package all-the-icons)
 
-;; Which Key
+
+;; Which Key (tuş kombinasyonları gösterimi)
 (use-package which-key
   :init (which-key-mode)
   :config
   (setq which-key-idle-delay 0.3))
 
-;; Dashboard
+;; Dashboard Yapılandırması
 (use-package dashboard
   :init
-  (setq initial-buffer-choice (lambda () (get-buffer "*dashboard*"))
-  (setq dashboard-startup-banner "~/.emacs.d/logo.txt")
+  (setq initial-buffer-choice (lambda () (get-buffer "*dashboard*")))
+  (setq dashboard-startup-banner "~/.emacs.d/logo.txt") ;; logo.txt dosyasını göster
   (setq dashboard-center-content t)
   (setq dashboard-items '((recents  . 5)
                           (bookmarks . 3)
@@ -113,7 +127,8 @@
 (setq dashboard-set-file-icons t)
 (setq dashboard-set-heading-icons t)
 
-;; Ivy/Counsel
+
+;; Ivy/Counsel (Telescope benzeri dosya arama)
 (use-package ivy
   :init (ivy-mode 1)
   :config
@@ -126,11 +141,23 @@
   :bind (("C-x C-f" . counsel-find-file)
          ("M-x" . counsel-M-x)))
 
+
 (use-package vterm
   :ensure t
   :commands (vterm)
   :config
   (setq vterm-max-scrollback 10000))
+  ;; (setq buffer-read-only t)
+
+
+;; (defun my/toggle-vterm ()
+;;   "Toggle a vterm buffer."
+;;   (interactive)
+;;   (if (get-buffer "*vterm*")
+;;       (if (eq (current-buffer) (get-buffer "*vterm*"))
+;;           (previous-buffer)
+;;         (switch-to-buffer "*vterm*"))
+;;     (vterm)))
 
 (defun my/toggle-vterm ()
   "Toggle a vterm buffer."
@@ -140,15 +167,20 @@
           (previous-buffer)
         (progn
           (switch-to-buffer "*vterm*")
-          (read-only-mode -1)))
+          (read-only-mode -1))) ;; bu satırı ekle
     (vterm)))
+
+
+
 
 (defun my/open-or-create-notes-org ()
   "Open notes.org in current directory, creating it if necessary."
   (interactive)
   (let ((notes-path (expand-file-name "notes.org" default-directory)))
+    ;; Dosya yoksa oluştur
     (unless (file-exists-p notes-path)
       (with-temp-buffer (write-file notes-path)))
+    ;; Dosyayı aç
     (find-file notes-path)))
 
 (defadvice counsel-find-file (after protect-buffers activate)
@@ -156,9 +188,10 @@
   (unless (file-exists-p (ad-get-arg 0))
     (find-file (ad-get-arg 0))))
 
+
 (use-package treesit-auto
   :custom
-  (treesit-auto-install 'prompt)
+  (treesit-auto-install 'prompt) ;; parser eksikse sorar
   :config
   (treesit-auto-add-to-auto-mode-alist 'all)
   (global-treesit-auto-mode))
@@ -170,7 +203,8 @@
       (comment-or-uncomment-region (region-beginning) (region-end))
     (comment-line 1)))
 
-;; Leader key ayarları
+
+;; Leader key ayarları (SPC = boşluk tuşu
 (use-package general
   :after evil
   :config
@@ -193,8 +227,6 @@
     "fo" '(counsel-recentf :wk "Recent files")
     "o" '(:wk "Org")
     "oo" '(my/open-or-create-notes-org :wk "Create notes/open notes")
-    "oa" '(org-agenda :wk "Open agenda")  ;; Org-agenda kısayolu eklendi
-    "oc" '(org-capture :wk "Capture note")  ;; Org-capture kısayolu eklendi
     "l" '(:wk "Lsp")
     "ll" '(lsp :wk "Activate lsp")
     "zA" '(vimish-fold :wk "Create fold")
@@ -202,40 +234,23 @@
     "tn" '(display-line-numbers-mode :wk "Toggle line numbers")
     "tm" '(hide-mode-line-mode :wk "Toggle mode-line")))
 
-;; Recentf
+
+
+;; Recentf (son açılan dosyalar)
 (use-package recentf
   :config
   (recentf-mode 1)
   (setq recentf-max-menu-items 50
         recentf-max-saved-items 50))
 
-;; Org mode ve Org-agenda ayarları
+;; Org mode
 (use-package org
   :hook (org-mode . org-indent-mode)
   :config
   (setq org-hide-emphasis-markers t
         org-startup-indented t
         org-tempo t
-        org-pretty-entities t
-        ;; Org-agenda ayarları
-        org-agenda-files '("~/org/")  ;; Org dosyalarınızın bulunduğu dizin
-        org-refile-targets '((nil :maxlevel . 3)
-                             (org-agenda-files :maxlevel . 3))
-        org-todo-keywords '((sequence "TODO(t)" "INPROGRESS(i)" "|" "DONE(d)" "CANCELED(c)"))
-        org-log-done 'time
-        org-agenda-start-with-log-mode t
-        org-agenda-span 'week
-        org-agenda-start-on-weekday 1
-        org-agenda-custom-commands
-        '(("d" "Günlük agenda"
-           ((agenda "" ((org-agenda-span 1)
-                        (org-agenda-overriding-header "Günlük Görevler"))))
-          ("w" "Haftalık agenda"
-           ((agenda "" ((org-agenda-span 7)
-                        (org-agenda-overriding-header "Haftalık Görevler")))))
-        org-capture-templates
-        '(("t" "Todo" entry (file+headline "~/org/tasks.org" "Görevler")
-           "* TODO %?\n  %u\n  %a")))
+        org-pretty-entities t))
 
 (use-package org-bullets
   :hook (org-mode . org-bullets-mode)
@@ -248,28 +263,35 @@
  '((emacs-lisp . t)
    (python . t)
    (C . t)
-   (shell . t)))
+   (shell . t))) ;; örnek diller, istediğini ekle
 
-(setq org-confirm-babel-evaluate nil)
+(setq org-confirm-babel-evaluate nil)  ;; Her seferinde "emin misin?" demesin
 
 ;; Org-mode Görsel Ayarları
-(setq org-fontify-done-headers t
-      org-auto-formatting t
-      org-list-demote-modify-bullet t
-      org-checkbox-hierarchical-statistics nil
-      org-log-done 'time)
+(setq org-fontify-done-headers t)                ;; Tamamlananları soluk göster
+(setq org-auto-formatting t)                    ;; Otomatik formatlama
+(setq org-list-demote-modify-bullet t)           ;; Madde işaretlerini güncelle
 
+(setq org-checkbox-hierarchical-statistics nil) ;; alt başlıklardan etkilenmesin istersen
+(setq org-log-done 'time) ;; DONE olduğunda zaman eklesin
+
+;; Alt kutucuklar güncellendikçe üst başlıktaki [/], [%] güncellensin
 (add-hook 'org-checkbox-statistics-hook #'org-update-statistics-cookies)
 
 (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(org-checkbox-statistics-done ((t (:foreground "gray50" :strike-through t))))
  '(org-done ((t (:foreground "gray60" :strike-through t)))))
+
 
 (use-package autoinsert
   :ensure nil
   :hook (find-file . auto-insert)
   :custom
-  (auto-insert-query nil)
+  (auto-insert-query nil) ; otomatik olsun, sormasın
   :config
   (define-auto-insert
     "\\.org\\'"
@@ -278,9 +300,12 @@
         (insert (format "#+title: %s\n" filename))
         (insert (format "#+date: %s\n\n" (format-time-string "%Y-%m-%d")))))))
 
+
+
 (use-package yasnippet
   :config
   (yas-global-mode 1))
+
 
 ;; LSP ve tamamlama
 (use-package company
@@ -291,7 +316,7 @@
 
 (use-package lsp-mode
   :init
-  (setq lsp-keymap-prefix "C-c l")
+  (setq lsp-keymap-prefix "C-c l")  ; LSP komutları için önek
   :hook (
          (c-mode . lsp)
          (c++-mode . lsp)
@@ -299,9 +324,10 @@
          (lsp-mode . lsp-enable-which-key-integration))
   :commands lsp
   :config
-  (setq lsp-completion-provider :capf
-        lsp-auto-guess-root t
-        lsp-restart 'auto-restart))
+  (setq lsp-completion-provider :capf)
+  ;; LSP otomatik başlatma için ek ayar
+  (setq lsp-auto-guess-root t)  ; Proje kökünü otomatik tahmin et
+  (setq lsp-restart 'auto-restart))  ; Gerekirse otomatik yeniden başlat
 
 (use-package lsp-ui
   :commands lsp-ui-mode
@@ -312,7 +338,7 @@
 
 ;; Python geliştirme ortamı
 (use-package python
-  :ensure nil
+  :ensure nil ; Built-in paketi kullan
   :mode ("\\.py\\'" . python-mode)
   :interpreter ("python" . python-mode)
   :config
@@ -324,20 +350,27 @@
 
 ;; C/C++ geliştirme
 (use-package cc-mode
-  :ensure nil
+  :ensure nil ; Built-in paketi kullan
   :hook ((c-mode c++-mode) . lsp))
 
-;; clangd ayarları
+;; clangd ayarları (lsp-mode ile entegre)
 (setq lsp-clients-clangd-executable "clangd"
       lsp-clients-clangd-args '("--background-index"
                                "--clang-tidy"
                                "--completion-style=detailed"
                                "--header-insertion=never"))
 
+
+;; C/C++ için lsp-clangd otomatik başlatma
 (add-hook 'c-mode-hook 'lsp-deferred)
 (add-hook 'c++-mode-hook 'lsp-deferred)
 
+
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("f5f80dd6588e59cfc3ce2f11568ff8296717a938edd448a947f9823a4e282b66"
      default))
