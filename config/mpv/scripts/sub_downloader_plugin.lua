ca51647d1c67b8ed7320fbeef968f53f57178a98
OsmanFrat <osmanfrat@yahoo.com>
2025-05-04 13:38:00 +0300

    Auto commit - Sun May  4 01:38:00 PM +03 2025

---
+++ config/mpv/scripts/sub_downloader_plugin.lua
@@ -0,0 +1,59 @@
+local utils = require 'mp.utils'
+
+function display_error()
+  mp.msg.warn("Subtitle download failed: ")
+  mp.osd_message("Subtitle download failed")
+end
+
+-- ost download --file Dumbo.1941.mp4
+function load_sub_with_hash()
+  local path = mp.get_property("path")
+  local dir, filename = utils.split_path(path)
+  local sub_name = string.gsub(filename, "%.%w+$", "")
+  local srt_path = string.gsub(path, "%.%w+$", ".srt")
+  mp.osd_message("Searching subtitle...")
+
+  if os.execute("cd '"..dir.."' && ost download --file "..filename.." && mv "..sub_name..".srt '"..srt_path.."'") then
+      if mp.commandv("sub_add", srt_path) then
+        mp.msg.warn("Subtitle download succeeded")
+        mp.osd_message("Subtitle download succeeded: \n" .. srt_path .. "", 5)
+      end
+      else
+        display_error()
+      end
+end
+
+mp.add_key_binding("a", "sub_downloader_plugin", load_sub_with_hash)
+
+--[[
+local utils = require 'mp.utils'
+
+function display_error()
+  mp.msg.warn("Subtitle download failed: ")
+  mp.osd_message("Subtitle download failed")
+end
+
+function load_sub_fn()
+  local path = mp.get_property("path")
+  local dir, filename = utils.split_path(path)
+  mp.osd_message("Searching subtitle...")
+  local id = io.popen("ost search --query '"..filename.."' | awk -F '|' 'NR==4 {print $5}'"):read("*number")
+  if id then
+    local srt_path = string.gsub(path, "%.%w+$", ".srt")
+    if os.execute("cd '"..dir.."' && ost download --file-id "..id.." && mv "..id..".srt '"..srt_path.."'") then
+      if mp.commandv("sub_add", srt_path) then
+        mp.msg.warn("Subtitle download succeeded")
+        mp.osd_message("Subtitle download succeeded: \n" .. srt_path .. "", 5)
+      else
+        display_error()
+      end
+    else
+      display_error()
+    end
+  else
+    display_error()
+  end
+end
+
+mp.add_key_binding("a", "auto_load_subs", load_sub_fn)
+]]
