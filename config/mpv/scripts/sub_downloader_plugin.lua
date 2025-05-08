6126294ea494a25d258ae7f82d6e9363149f05d7
OsmanFrat <osmanfrat@yahoo.com>
2025-05-08 23:38:00 +0300

    Auto commit - Thu May  8 11:38:00 PM +03 2025

--- config/mpv/scripts/sub_downloader_plugin.lua
+++ config/mpv/scripts/sub_downloader_plugin.lua
@@ -1,66 +1,26 @@
 local utils = require 'mp.utils'
 
--- Basit hata bildirimi
-local function display_error()
-  mp.msg.warn("Subtitle download failed.")
-  mp.osd_message("Subtitle download failed", 3)
+function display_error()
+  mp.msg.warn("Subtitle download failed: ")
+  mp.osd_message("Subtitle download failed")
 end
 
--- Tüm .srt dosyalarını mevcut klasörden bulur
-local function find_all_srt_files(dir)
-  local files = utils.readdir(dir, "files") or {}
-  local srt_files = {}
-
-  for _, file in ipairs(files) do
-    if file:match("%.srt$") then
-      table.insert(srt_files, utils.join_path(dir, file))
-    end
-  end
-
-  return srt_files
-end
-
--- Tüm altyazı dosyalarını ekler
-local function add_all_subs(dir)
-  local srt_files = find_all_srt_files(dir)
-
-  if #srt_files == 0 then
-    mp.osd_message("No subtitles found.", 3)
-    return
-  end
-
-  for _, sub_file in ipairs(srt_files) do
-    mp.commandv("sub-add", sub_file, "auto")
-  end
-
-  mp.msg.info("All subtitles added.")
-  mp.osd_message("Subtitles loaded: " .. tostring(#srt_files), 3)
-end
-
--- Asıl fonksiyon: ozusub ile indir, sonra ekle
-local function load_all_subs()
+-- ost download --file Dumbo.1941.mp4
+function load_sub_with_hash()
   local path = mp.get_property("path")
-  if not path then
-    mp.osd_message("No file loaded.", 2)
-    return
-  end
-
   local dir, filename = utils.split_path(path)
+  local sub_name = string.gsub(filename, "%.%w+$", "")
+  local srt_path = string.gsub(path, "%.%w+$", ".srt")
+  mp.osd_message("Searching subtitle...")
 
-  mp.osd_message("Downloading subtitles...", 2)
-  local command = "cd '" .. dir .. "' && ozusub -m \"" .. filename .. "\""
-  -- file name test
-  mp.osd_message(filename)
-  local result = os.execute(command)
-
-  if result == 0 then
-    mp.osd_message("Download complete. Loading subtitles...", 2)
-    add_all_subs(dir)
-  else
-    display_error()
-  end
+  if os.execute("cd '"..dir.."' && ost download --file "..filename.." && mv "..sub_name..".srt '"..srt_path.."'") then
+      if mp.commandv("sub_add", srt_path) then
+        mp.msg.warn("Subtitle download succeeded")
+        mp.osd_message("Subtitle download succeeded: \n" .. srt_path .. "", 5)
+      end
+      else
+        display_error()
+      end
 end
 
--- "a" tuşuna basınca `load_all_subs()` çalışsın
-mp.add_key_binding("a", "sub_downloader_plugin", load_all_subs)
-
+mp.add_key_binding("a", "sub_downloader_plugin", load_sub_with_hash)
