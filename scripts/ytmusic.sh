a8f9274370d083546bd953bf00e0de24c33d18a8
OsmanFrat <osmanfrat@yahoo.com>
2025-05-20 11:16:00 +0300

    Auto commit - Tue May 20 11:16:00 AM +03 2025

--- scripts/ytmusic.sh
+++ scripts/ytmusic.sh
@@ -4,6 +4,6 @@ yt-dlp --flat-playlist --get-url "https://www.youtube.com/playlist?list=PLTRI6Hj
 
 while true; do
   video_url=$(shuf -n 1 /home/ozu/dotfiles/scripts/playlist.txt)
-  echo "Şimdi çalınıyor: $video_url"
+  echo "Playing: $video_url"
   mpv --no-video --ytdl-format="bestaudio" "$video_url"
 done
