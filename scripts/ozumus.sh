60a95a6f209f4afa41b4db0938db5f39cd04ad0a
OsmanFrat <osmanfrat@yahoo.com>
2025-05-29 09:50:00 +0300

    Auto commit - Thu May 29 09:50:00 AM +03 2025

--- scripts/ozumus.sh
+++ scripts/ozumus.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-# without an argument - just update local songs and play in a random order
-if [ $# -eq 0 ]; then
+function update_local_song_and_play {
   cd "$HOME/Music" || { echo "Error: Could not cd to $HOME/Muzic"; exit 1; }
   mpc clear
   mpc add /
   mpc play
+}
+
+
+# without an argument - just update local songs and play in a random order
+if [ $# -eq 0 ]; then
+  update_local_song_and_play
   exit 0
 fi
 
@@ -16,6 +21,7 @@ while getopts ":r" opt; do
       # run ./update-playlist.sh if -r is passed
       if [ -f "$HOME/dotfiles/scripts/update-playlist.sh" ]; then
         $HOME/dotfiles/scripts/update-playlist.sh
+        update_local_song_and_play
       else
         echo "Error: $HOME/dotfiles/scripts/update-playlist.sh is not found!"
         exit 1
