f4acf812706938f0585c4ac24d2c086e3e65cd1b
OsmanFrat <osmanfrat@yahoo.com>
2025-05-29 09:28:00 +0300

    Auto commit - Thu May 29 09:28:00 AM +03 2025

--- scripts/ozumus.sh
+++ scripts/ozumus.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
+# without an argument
+if [ $# -eq 0 ]; then
+  cd "$HOME/Music"
+  mpc clear
+  mpc add /
+  mpc play
+  exit 0
+fi
+
+
 cd $HOME/dotfiles/scripts
 ./update-playlist.sh
 cd $HOME/Music
 mpc clear
 mpc add /
 mpc play
+
+
+
