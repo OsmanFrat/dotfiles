4d997a392512966794b36a83b1a8687e90b267fa
OsmanFrat <osmanfrat@yahoo.com>
2025-06-08 17:14:00 +0300

    Auto commit - Sun Jun  8 05:14:00 PM +03 2025

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -3,3 +3,24 @@ require("core.lsp")
 require("config.options")
 require("config.keymaps")
 require("config.utils")
+
+-- neovide test
+-- Neovide'da Ctrl + Fare Tekerleği ile yakınlaştırma
+if vim.g.neovide then
+	vim.g.neovide_scale_factor = 1.0 -- Başlangıç boyutu (1.0 = %100)
+
+	-- Ctrl + Tekerlek Yukarı → Yakınlaştır
+	vim.keymap.set({ "n", "v" }, "<C-ScrollWheelUp>", function()
+		vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1
+	end, { silent = true })
+
+	-- Ctrl + Tekerlek Aşağı → Uzaklaştır
+	vim.keymap.set({ "n", "v" }, "<C-ScrollWheelDown>", function()
+		vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1
+	end, { silent = true })
+
+	-- Bonus: Ctrl+0 ile sıfırla (Opsiyonel)
+	vim.keymap.set("n", "<C-0>", function()
+		vim.g.neovide_scale_factor = 1.0
+	end, { silent = true })
+end
