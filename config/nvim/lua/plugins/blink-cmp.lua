2fdbd3fef834447650288931d4aa425ca1124223
OsmanFrat <osmanfrat@yahoo.com>
2025-06-08 22:46:00 +0300

    Auto commit - Sun Jun  8 10:46:00 PM +03 2025

--- config/nvim/lua/plugins/blink-cmp.lua
+++ config/nvim/lua/plugins/blink-cmp.lua
@@ -8,7 +8,7 @@ return {
 			["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
 			["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
 			["<CR>"] = { "accept", "fallback" },
-			["<Esc>"] = { "hide", "fallback" },
+			-- ["<Esc>"] = { "hide", "fallback" },
 			["<PageUp>"] = { "scroll_documentation_up", "fallback" },
 			["<PageDown>"] = { "scroll_documentation_down", "fallback" },
 		},
