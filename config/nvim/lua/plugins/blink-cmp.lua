b35959b7232b033211f2a03210f9819a57d54b79
OsmanFrat <osmanfrat@yahoo.com>
2025-06-08 21:22:01 +0300

    Auto commit - Sun Jun  8 09:22:00 PM +03 2025

--- config/nvim/lua/plugins/blink-cmp.lua
+++ config/nvim/lua/plugins/blink-cmp.lua
@@ -1,35 +1,37 @@
 return {
-  'saghen/blink.cmp',
-  dependencies = { 'rafamadriz/friendly-snippets' },
-  version = '1.*',
-  opts = {
-    -- Özel keymap tanımlıyoruz
-    keymap = {
-      ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
-      ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
-      ["<CR>"] = { "accept", "fallback" },
-      ["<Esc>"] = { "hide", "fallback" },
-      ["<PageUp>"] = { "scroll_documentation_up", "fallback" },
-      ["<PageDown>"] = { "scroll_documentation_down", "fallback" },
-    },
+	"saghen/blink.cmp",
+	dependencies = { "rafamadriz/friendly-snippets" },
+	version = "1.*",
+	opts = {
+		-- Özel keymap tanımlıyoruz
+		keymap = {
+			["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+			["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
+			["<CR>"] = { "accept", "fallback" },
+			["<Esc>"] = { "hide", "fallback" },
+			["<PageUp>"] = { "scroll_documentation_up", "fallback" },
+			["<PageDown>"] = { "scroll_documentation_down", "fallback" },
+		},
 
-    appearance = {
-      nerd_font_variant = 'mono'
-    },
+		appearance = {
+			nerd_font_variant = "mono",
+		},
 
-    completion = {
-      documentation = { auto_show = false }
-    },
+		completion = {
+			documentation = { auto_show = false },
+			-- Aşağıdaki ayarı ekleyin
+			preselect = false, -- veya false da olabilir
+		},
 
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-    },
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+			preselect = false,
+		},
 
-    fuzzy = {
-      implementation = "prefer_rust_with_warning"
-    }
-  },
+		fuzzy = {
+			implementation = "prefer_rust_with_warning",
+		},
+	},
 
-  opts_extend = { "sources.default" }
+	opts_extend = { "sources.default" },
 }
-
