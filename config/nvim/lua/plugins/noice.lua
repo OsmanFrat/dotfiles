be342422da3f6676caf3d95550284a8a81137bf2
OsmanFrat <osmanfrat@yahoo.com>
2025-06-08 19:46:00 +0300

    Auto commit - Sun Jun  8 07:46:00 PM +03 2025

--- config/nvim/lua/plugins/noice.lua
+++ config/nvim/lua/plugins/noice.lua
@@ -1,16 +1,23 @@
-return
-{
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- add any options here
-  },
-  dependencies = {
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-    "MunifTanjim/nui.nvim",
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
-    "rcarriga/nvim-notify",
-    }
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {
+		-- Bildirim süresini 1000ms (1 saniye) olarak ayarla
+		views = {
+			notify = {
+				timeout = 1000,
+			},
+		},
+		-- İsterseniz routes kısmında özel bir timeout da belirleyebilirsiniz
+		routes = {
+			{
+				filter = { event = "notify" },
+				opts = { timeout = 1000 },
+			},
+		},
+	},
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
+	},
 }
