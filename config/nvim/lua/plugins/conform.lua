1a620a5274f5910e267493748edae8b539e8ebd3
OsmanFrat <osmanfrat@yahoo.com>
2025-06-12 22:16:00 +0300

    Auto commit - Thu Jun 12 10:16:00 PM +03 2025

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -1,9 +1,16 @@
-      c = { 
-        { "clang_format", 
-          args = { "-style=\"{BreakBeforeBraces: Allman}\"" } 
-        } 
-      },
-      cpp = { 
-        { "clang_format", 
-          args = { "-style=\"{BreakBeforeBraces: Allman}\"" } 
-        } 
+return {
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			python = { "black" }, -- önce isort, sonra black
+			lua = { "stylua" },
+			c = { "clang_format" },
+			cpp = { "clang_format" },
+		},
+		-- kaydederken otomatik formatla
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
+	},
+}
