05975aaa66eae60e0595713fd2f226539848389f
OsmanFrat <osmanfrat@yahoo.com>
2025-12-22 15:18:00 +0300

    Auto commit - Mon Dec 22 03:18:00 PM +03 2025

--- config/nvim/lua/plugins/render-markdown.lua
+++ config/nvim/lua/plugins/render-markdown.lua
@@ -7,4 +7,26 @@ return
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
     opts = {},
+    indent = {
+        -- Mimic org-indent-mode behavior by indenting everything under a heading based on the
+        -- level of the heading. Indenting starts from level 2 headings onward by default.
+
+        -- Turn on / off org-indent-mode.
+        enabled = false,
+        -- Additional modes to render indents.
+        render_modes = false,
+        -- Amount of additional padding added for each heading level.
+        per_level = 2,
+        -- Heading levels <= this value will not be indented.
+        -- Use 0 to begin indenting from the very first level.
+        skip_level = 1,
+        -- Do not indent heading titles, only the body.
+        skip_heading = false,
+        -- Prefix added when indenting, one per level.
+        icon = '▎',
+        -- Priority to assign to extmarks.
+        priority = 0,
+        -- Applied to icon.
+        highlight = 'RenderMarkdownIndent',
+    },
 }
