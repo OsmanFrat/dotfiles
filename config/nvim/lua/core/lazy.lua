30d71333eb0420b69439dafc77d71ede516959f6
OsmanFrat <osmanfrat@yahoo.com>
2025-12-22 14:50:01 +0300

    Auto commit - Mon Dec 22 02:50:01 PM +03 2025

--- config/nvim/lua/core/lazy.lua
+++ config/nvim/lua/core/lazy.lua
@@ -45,6 +45,8 @@ require("lazy").setup({
 		--		{ import = "plugins.conform" },
 		{ import = "plugins.neorg" },
 		{ import = "plugins.plenary" },
+        { import = "render-markdown"},
+        { import = "mini-icon"},
 	},
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
