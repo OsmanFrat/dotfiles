32dd1fdc6206f281bd1c105f45dd02d0a17031ac
OsmanFrat <osmanfrat@yahoo.com>
2025-12-22 15:30:00 +0300

    Auto commit - Mon Dec 22 03:30:00 PM +03 2025

--- config/nvim/lua/plugins/mini-icons.lua
+++ config/nvim/lua/plugins/mini-icons.lua
@@ -1,18 +1,25 @@
-return
-{
-  'nvim-mini/mini.icons', version = false,
-  -- Icon style: 'glyph' or 'ascii'
-  style = 'glyph',
 
-  -- Customize per category. See `:h MiniIcons.config` for details.
-  default   = {},
-  directory = {},
-  extension = {},
-  file      = {},
-  filetype  = {},
-  lsp       = {},
-  os        = {},
+return {
+  'nvim-mini/mini.icons',
+  version = false,
+  opts = {
+    -- Icon style: 'glyph' or 'ascii'
+    style = 'glyph',
 
-  -- Control which extensions will be considered during "file" resolution
-  use_file_extension = function(md, markdown) return true end,
+    -- Customize per category
+    default   = {},
+    directory = {},
+    extension = {},
+    file      = {},
+    filetype  = {},
+    lsp       = {},
+    os        = {},
+
+    -- Control which extensions will be considered during "file" resolution
+    use_file_extension = function(ext, file)
+    -- use_file_extension = function(md, markdown)
+      return true
+    end,
+  },
 }
+
