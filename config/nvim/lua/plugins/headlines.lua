3d90e47be32408cd8f1c2a55427c40f93ba2f444
OsmanFrat <osmanfrat@yahoo.com>
2025-12-28 15:28:01 +0300

    Auto commit - Sun Dec 28 03:28:01 PM +03 2025

--- config/nvim/lua/plugins/headlines.lua
+++ config/nvim/lua/plugins/headlines.lua
@@ -1,53 +1,50 @@
 return {
-  "lukas-reineke/headlines.nvim",
-  dependencies = { "nvim-treesitter/nvim-treesitter" },
-  opts = {
-    org = {
-      query = [[
-        (headline (stars) @headline)
+        "lukas-reineke/headlines.nvim",
+        dependencies = "nvim-treesitter/nvim-treesitter",
+        config = true, -- or `opts = {}`
+        opts = {
+           org = {
+            query = vim.treesitter.parse_query(
+                "org",
+                [[
+                    (headline (stars) @headline)
 
-        (
-          (expr) @dash
-          (#match? @dash "^-----+$")
-        )
+                    (
+                        (expr) @dash
+                        (#match? @dash "^-----+$")
+                    )
 
-        (block
-          name: (expr) @_name
-          (#match? @_name "(SRC|src)")
-        ) @codeblock
+                    (block
+                        name: (expr) @_name
+                        (#match? @_name "(SRC|src)")
+                    ) @codeblock
 
-        (paragraph . (expr) @quote
-          (#eq? @quote ">")
-        )
-      ]],
-
-      headline_highlights = { "Headline" },
-
-      bullet_highlights = {
-        "@org.headline.level1",
-        "@org.headline.level2",
-        "@org.headline.level3",
-        "@org.headline.level4",
-        "@org.headline.level5",
-        "@org.headline.level6",
-        "@org.headline.level7",
-        "@org.headline.level8",
-      },
-
-      bullets = { "◉", "○", "✿", "✸" },
-
-      codeblock_highlight = "CodeBlock",
-
-      dash_highlight = "Dash",
-      dash_string = "-",
-
-      quote_highlight = "Quote",
-      quote_string = "┃",
-
-      fat_headlines = true,
-      fat_headline_upper_string = "▄",
-      fat_headline_lower_string = "▀",
-    },
-  },
+                    (paragraph . (expr) @quote
+                        (#eq? @quote ">")
+                    )
+                ]]
+            ),
+            headline_highlights = { "Headline" },
+            bullet_highlights = {
+                "@org.headline.level1",
+                "@org.headline.level2",
+                "@org.headline.level3",
+                "@org.headline.level4",
+                "@org.headline.level5",
+                "@org.headline.level6",
+                "@org.headline.level7",
+                "@org.headline.level8",
+            },
+            bullets = { "◉", "○", "✿", "✸" },
+            codeblock_highlight = "CodeBlock",
+            dash_highlight = "Dash",
+            dash_string = "-",
+            quote_highlight = "Quote",
+            quote_string = "┃",
+            fat_headlines = true,
+            fat_headline_upper_string = "▄",
+            fat_headline_lower_string = "▀",
+        },
+      }
 }
 
