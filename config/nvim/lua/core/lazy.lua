9fd2d91597f22a263eb1225bffbc64fdb96e25a9
OsmanFrat <osmanfrat@yahoo.com>
2025-12-28 15:40:00 +0300

    Auto commit - Sun Dec 28 03:40:00 PM +03 2025

--- config/nvim/lua/core/lazy.lua
+++ config/nvim/lua/core/lazy.lua
@@ -49,7 +49,7 @@ require("lazy").setup({
         -- { import = "plugins.mini-icons"},
 		{ import = "plugins.nvim-orgmode" },
         { import = "plugins.org-bullets"},
-        { import = "plugins.headlines"}
+        -- { import = "plugins.headlines"}
 	},
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
