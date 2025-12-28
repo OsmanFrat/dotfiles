5380beab624789f38f454c6a2f9e1d4301060a45
OsmanFrat <osmanfrat@yahoo.com>
2025-12-28 14:58:00 +0300

    Auto commit - Sun Dec 28 02:58:00 PM +03 2025

--- config/nvim/lua/plugins/nvim-orgmode.lua
+++ config/nvim/lua/plugins/nvim-orgmode.lua
@@ -7,7 +7,7 @@ return {
     require('orgmode').setup({
       -- org_agenda_files = '~/orgfiles/**/*',
       -- org_default_notes_file = '~/orgfiles/refile.org',
-      org_agenda_files = '~/github/notes/agenda/**/*',
+      org_agenda_files = '~/github/notes/**/*.org',
       org_default_notes_file = '~/orgfiles/refile.org',
 
     })
